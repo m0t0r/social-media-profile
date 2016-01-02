13913d37b88b32f9dcd6e828bd386885c5e605d3
m0t0r <tkachenko.vitaly.job@gmail.com>
2016-01-02 11:28:06 -0500

    chore(app): Load semantic-ui files

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
   devtools: 'source-map',
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel",  query: { presets: ['es2015']}}
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel",  query: { presets: ['es2015']}},
+      { test: /\.css/, exclude: /node_modules/, loader: "style-loader!css-loader"}
     ]
   },
   resolve: ['', '.js']
